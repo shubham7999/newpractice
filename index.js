6e59dc031e2259f3b12f02045159147ec235d7d0
Shubham <shubhampokhriyal2018@gmail.com>
2021-09-12 10:27:15 +0530

    get by classname

--- index.js
+++ index.js
@@ -7,8 +7,20 @@ console.log(document.title);
 
 console.log(document.all);
 
+
+// Get elelemnt by id
 console.log(document.getElementById('start'));
 
 let header = document.getElementById('start');
 
 header.style.border = 'solid 3px black';
+
+// Get element by class name
+let lists = document.getElementsByClassName('listitem');
+console.log(lists[0])
+
+lists[0].style.fontWeight = 'bold';
+lists[1].style.fontWeight = 'bold';
+lists[2].style.fontWeight = 'bold';
+
+lists[2].style.backgroundColor = 'green';
