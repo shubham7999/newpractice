8b2fd015cbcd75eb15933ffd64fa4f49540c2271
Shubham <shubhampokhriyal2018@gmail.com>
2021-09-12 10:18:24 +0530

    header getelement by id

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 console.log("Started");
 
+console.log(document);
+console.log(document.domain);
+console.log(document.URL);
+console.log(document.title);
+
+console.log(document.all);
+
+console.log(document.getElementById('start'));
+
+let header = document.getElementById('start');
+
+header.style.border = 'solid 3px black';
