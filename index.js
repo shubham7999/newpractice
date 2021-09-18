675db08d93dca5cf8895fda82be6b226667af8b8
Shubham <shubhampokhriyal2018@gmail.com>
2021-09-18 14:23:45 +0530

    added user addition in localstorage

--- index.js
+++ index.js
@@ -2,13 +2,34 @@ const button = document.querySelector('button');
 
 button.addEventListener('click' , ()=>{
 
-    let name = document.getElementById('name').value;
-    let phone = document.getElementById('phone').value;
-    let email = document.getElementById('email').value;
 
-    localStorage.setItem("name", name);
-    localStorage.setItem("phone", phone);
-    localStorage.setItem("email", email);
+    var obj = {};
 
+    obj.name = document.getElementById('name').value;
+    obj.phone = document.getElementById('phone').value;
+    obj.email = document.getElementById('email').value;
+
+    let set = JSON.stringify(obj);
+
+    let cnt = localStorage.length;
+
+    localStorage.setItem('user' + cnt , set);
     
 });
+
+var items = {...localStorage};
+
+let html = ``;
+
+for(let key in items)
+{
+    let obj = JSON.parse(items[key]);
+    html += `<li> User name is  ${obj.name} , User email is ${obj.email} ,  User phone is ${obj.phone} </li>`
+
+}
+
+var root = document.getElementById('root');
+console.log(root);
+root.innerHTML = html;
+
+
